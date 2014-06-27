cb159d1ff7e67716998f9ed19fc295726a6f1fee
smo93 <georgimpeev@gmail.com>
2014-06-28 00:15:40 +0300

    refactored some duplicating code

--- 2/app.js
+++ 2/app.js
@@ -27,47 +27,35 @@ $(document).ready(function() {
         $('#second-second-digit').text((time[1] % 10).toString());
     }
 
-    $('#count-up').on('click', function() {
-        clearInterval(intervalId);
+    $('#count-up').on('click', { timeHandler: increaseTime, startTime: [0, 0], finishTime: "user input", message: "Time reached!" }, startTimer);
 
-        time = [0, 0];
-        userTime = [parseInt($('#minutes').val()), parseInt($('#seconds').val())];
-        if(isNaN(userTime[0])) { userTime[0] = 0; }
-        if(isNaN(userTime[1])) { userTime[1] = 0; }
+    $('#count-down').on('click', { timeHandler: decreaseTime, startTime: "user input", finishTime: [0, 0], message: "Time's up!" }, startTimer);
 
-        updateTime();
-
-        intervalId = setInterval(function() {
-            increaseTime();
-            updateTime();
-
-            if(time[0] === userTime[0] && time[1] === userTime[1]) {
-                alert('Hooray!');
-                clearInterval(intervalId);
-            }
-        }, 1000);
-    });
-
-    $('#count-down').on('click', function() {
+    function startTimer(event) {
         clearInterval(intervalId);
 
         userTime = [parseInt($('#minutes').val()), parseInt($('#seconds').val())];
         if(isNaN(userTime[0])) { userTime[0] = 0; }
         if(isNaN(userTime[1])) { userTime[1] = 0; }
-        time = userTime;
+
+        if(event.data.startTime === "user input") { event.data.startTime = userTime; }
+        if(event.data.finishTime === "user input") { event.data.finishTime = userTime; }
+
+        time = event.data.startTime;
 
         updateTime();
 
         intervalId = setInterval(function() {
-            decreaseTime();
+            event.data.timeHandler();
             updateTime();
 
-            if(time[0] === 0 && time[1] === 0) {
-                alert('Ieiii! ^_^');
+            if(time[0] === event.data.finishTime[0] && time[1] === event.data.finishTime[1]) {
+                alert(event.data.message);
                 clearInterval(intervalId);
             }
         }, 1000);
-    });
+
+    }
 
     $('#stop').on('click', function() {
         clearInterval(intervalId);
